b045bc2fabc33389e8a048d8508e55514eb34712
elliemcavoy <ellie.mcavoy@outlook.com>
2022-01-08 23:38:12 +0000

    Model for menu and category type added and added to admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Menu, Category
 
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'description',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Category, CategoryAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
