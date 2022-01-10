fc5acfa9098e71c13e618acc03a2eea33e502e1a
elliemcavoy <ellie.mcavoy@outlook.com>
2022-01-10 19:11:50 +0000

    Added context processor for bag to be available on any page

---
+++ bag/contexts.py
@@ -0,0 +1,13 @@
+
+def bag_items(request):
+    bag_items = []
+    total = 0
+    item_count = 0
+    
+    context = {
+        bag_items
+        total
+        item_count
+    }
+
+    return context
