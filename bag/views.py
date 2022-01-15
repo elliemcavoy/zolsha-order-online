58dd710fd278f735b94961a3efe7ac5a0440a684
elliemcavoy <ellie.mcavoy@outlook.com>
2022-01-15 16:09:30 +0000

    Added options to add to bag view and displayed them in the success toast

--- bag/views.py
+++ bag/views.py
@@ -12,16 +12,19 @@ def add_to_bag(request, item_id):
     item = get_object_or_404(Menu, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    #if 'item_option' in request.POST:
-         #option = request.POST['item_option']
+    option = None
+    if 'item_option' in request.POST:
+         option = request.POST['item_option']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if option:
+        
+        bag[item_id] = {'items_by_option': {option: quantity}}
+        messages.success(request, f'Added {option.title()} {item.name} to your order')
+    
     else:
         bag[item_id] = quantity
-    
-    messages.success(request,f'Added {item.name}')
+        messages.success(request, f'Added {item.name} to your order')
 
     request.session['bag'] = bag
     print(request.session['bag'])
