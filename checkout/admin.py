b030617af8bc28dc3bc8825dffb77022f287eb00
elliemcavoy <ellie.mcavoy@outlook.com>
2022-01-16 13:42:01 +0000

    Added model for Order and form to gain detais from customer

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ('order_number', 'order_date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -7,8 +7,6 @@ from menu.models import Menu
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -16,8 +14,8 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    time = models.TimeField(auto_now_add=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_time = models.TimeField(auto_now_add=True)
     delivery_details = models.CharField(max_length=250, null=True, blank=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
