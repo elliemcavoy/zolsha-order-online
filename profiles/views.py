d49fd3bdbb2b227688e9b341e92cd0202607902c
elliemcavoy <ellie.mcavoy@outlook.com>
2022-01-22 23:54:45 +0000

    Added url ready for order history

--- profiles/views.py
+++ profiles/views.py
@@ -15,11 +15,34 @@ def profile(request):
             messages.success(request, 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
+    #if profile.orders:
+        #orders = profile.orders.all()
+    #else:
+    orders="No orders yet"
 
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
         'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
