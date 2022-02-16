5c63c47268c8d3c2df35e9c03ad4483d263e8b21
elliemcavoy <ellie.mcavoy@outlook.com>
2022-02-16 19:48:29 +0000

    Added styling to stripe payment element to match the other inputs on the page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -51,7 +51,7 @@ form.addEventListener('submit', function(ev) {
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
-    //$('#loading-overlay').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
     // From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
@@ -96,7 +96,7 @@ form.addEventListener('submit', function(ev) {
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
                 $('#payment-form').fadeToggle(100);
-                //$('#loading-overlay').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
