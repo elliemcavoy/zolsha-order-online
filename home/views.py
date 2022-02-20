736fea0225eb5156c2dd2d932abc4fad257e03df
elliemcavoy <ellie.mcavoy@outlook.com>
2022-02-20 22:24:08 +0000

    Added 404 error page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     """ View to render the index page """
     return render(request, 'home/index.html')
+
+
+def handler404(request, exception=None):
+    return render(request, '404.html')
